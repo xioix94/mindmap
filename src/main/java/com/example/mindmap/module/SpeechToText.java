fc55c7feafbd3ecb717978cc058a9520b6acce02
bagsl <bagsl@gmail.com>
2023-11-30 00:49:56 -0800

    feat: add SpeechToText module

---
+++ src/main/java/com/example/mindmap/module/SpeechToText.java
@@ -0,0 +1,48 @@
+package com.example.mindmap.module;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+//Imports the Google Cloud client library
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+
+public class SpeechToText {
+/** Demonstrates using the Speech API to transcribe an audio file. */
+	public static void syncRecognizeFile(String fileName) throws Exception {
+		try (SpeechClient speech = SpeechClient.create()) {
+			Path path = Paths.get(fileName);
+			byte[] data = Files.readAllBytes(path);
+			ByteString audioBytes = ByteString.copyFrom(data);
+
+			// Configure request with local raw PCM audio
+			RecognitionConfig config =
+					RecognitionConfig.newBuilder()
+					.setEncoding(AudioEncoding.LINEAR16)
+					.setLanguageCode("ko-KR")
+					.setSampleRateHertz(16000)
+					.build();
+			RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+		    // Use blocking call to get audio transcript
+		    RecognizeResponse response = speech.recognize(config, audio);
+		    List<SpeechRecognitionResult> results = response.getResultsList();
+
+		    for (SpeechRecognitionResult result : results) {
+		    	// There can be several alternative transcripts for a given chunk of speech. Just use the
+		      // first (most likely) one here.
+		      SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+		      System.out.printf("Transcription: %s%n", alternative.getTranscript());
+		    }
+		}
+	}
+}
---
+++ src/main/java/com/example/mindmap/module/main.java
@@ -0,0 +1,7 @@
+package com.example.mindmap.module;
+
+public class main {
+	public static void main(String[] args) throws Exception {
+		SpeechToText.syncRecognizeFile("/root/mindmap/test.wav");
+	}
+}
