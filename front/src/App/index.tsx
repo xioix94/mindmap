a49ca32f71d328c9760b505144dd1553538ded04
haaringa <xioix94@norma.co.kr>
2023-12-07 14:45:14 +0900

    feat: record

--- front/src/App/index.tsx
+++ front/src/App/index.tsx
@@ -1,5 +1,3 @@
-// index.tsx
-
 import React, { useState, useCallback, useRef } from 'react';
 import ReactFlow, {
   ConnectionLineType,
@@ -98,10 +96,17 @@ function Flow() {
   );
 
   const [recording, setRecording] = useState(false);
+  
+  const [stream, setStream] = useState<MediaStream | undefined>();
+  const [media, setMedia] = useState<MediaRecorder | undefined>();
+  const [onRec, setOnRec] = useState<boolean>(true);
+  const [source, setSource] = useState<MediaStreamAudioSourceNode | undefined>();
+  const [analyser, setAnalyser] = useState<ScriptProcessorNode | undefined>();
+  const [audioUrl, setAudioUrl] = useState<Blob | undefined>();
 
   const onStartClick = () => {
     const { checkedNode, startButtonPressed, setStartButtonPressed } = useStore.getState();
-
+    
     if (!checkedNode) {
       alert('체크된 노드가 없습니다!');
       return;
@@ -115,13 +120,86 @@ function Flow() {
     setStartButtonPressed(true);
     setRecording(true);
     console.log('Start button clicked!');
+
+    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
+    const audioCtx = new (window.AudioContext)();
+    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
+    const analyser = audioCtx.createScriptProcessor(0, 1, 1);
+    setAnalyser(analyser);
+
+    function makeSound(stream: MediaStream) {
+      // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
+      const source = audioCtx.createMediaStreamSource(stream);
+      setSource(source);
+      source.connect(analyser!);
+      analyser!.connect(audioCtx.destination);
+    }
+    // 마이크 사용 권한 획득
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((userStream) => {
+      const mediaRecorder = new MediaRecorder(userStream);
+      mediaRecorder.start();
+      setStream(userStream);
+      setMedia(mediaRecorder);
+      makeSound(userStream);
+
+      analyser!.onaudioprocess = function (e) {
+        // 3분(180초) 지나면 자동으로 음성 저장 및 녹음 중지
+        if (e.playbackTime > 180) {
+          userStream.getAudioTracks().forEach(function (track) {
+            track.stop();
+          });
+          mediaRecorder.stop();
+          // 메서드가 호출 된 노드 연결 해제
+          analyser!.disconnect();
+          audioCtx.createMediaStreamSource(userStream).disconnect();
+
+          mediaRecorder.ondataavailable = function (e) {
+            setAudioUrl(e.data);
+            setOnRec(true);
+          };
+        } else {
+          setOnRec(false);
+        }
+      };
+    });
   };
 
   const onStopClick = () => {
     useStore.getState().setStartButtonPressed(false);
     setRecording(false);
     console.log('Stop button clicked!');
+
+    // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
+    media!.ondataavailable = function (e) {
+      setAudioUrl(e.data);
+      setOnRec(true);
+    };
+
+    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
+    stream!.getAudioTracks().forEach(function (track) {
+      track.stop();
+    });
+
+    // 미디어 캡처 중지
+    media!.stop();
+    
+    // 메서드가 호출 된 노드 연결 해제
+    analyser!.disconnect();
+    source!.disconnect();
   };
+
+  const onSubmitAudioFile = useCallback(() => {
+
+    if (audioUrl) {
+      console.log(URL.createObjectURL(audioUrl)); // 출력된 링크에서 녹음된 오디오 확인 가능
+    }
+    // File 생성자를 사용해 파일로 변환
+    const sound = new File([audioUrl!], "soundBlob", {
+      lastModified: new Date().getTime(),
+      type: "audio",
+    });
+    console.log(sound); // File 정보 출력
+  }, [audioUrl]);
 
   return (
     <ReactFlow
